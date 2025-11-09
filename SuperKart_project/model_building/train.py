c0a86c5a05e1f09b56fd71e8771336c1766cea7a
shramik-dev <jsam333@gmail.com>
2025-11-09 15:38:51 +0000

    fix commit

--- SuperKart_project/model_building/train.py
+++ SuperKart_project/model_building/train.py
@@ -25,7 +25,7 @@ ytrain_path = "hf://datasets/Shramik121/superkart/ytrain.csv"
 ytest_path = "hf://datasets/Shramik121/superkart/ytest.csv"
 
 # Load data
-X trainable = pd.read_csv(Xtrain_path)
+X train = pd.read_csv(Xtrain_path)
 Xtest = pd.read_csv(Xtest_path)
 ytrain = pd.read_csv(ytrain_path).iloc[:, 0]
 ytest = pd.read_csv(ytest_path).iloc[:, 0]
