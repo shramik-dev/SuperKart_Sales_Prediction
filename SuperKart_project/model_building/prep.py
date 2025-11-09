efc0ab803455dcb45f18265931e886949580001b
shramik-dev <jsam333@gmail.com>
2025-11-09 13:36:58 +0000

    fix commit

--- SuperKart_project/model_building/prep.py
+++ SuperKart_project/model_building/prep.py
@@ -76,6 +76,6 @@ for file_path in files:
     api.upload_file(
         path_or_fileobj=file_path,
         path_in_repo=file_path.split("/")[-1],  # just the filename
-        repo_id="Shramik121/superkart-dataset",
+        repo_id="Shramik121/Superkart",
         repo_type="dataset",
     )
